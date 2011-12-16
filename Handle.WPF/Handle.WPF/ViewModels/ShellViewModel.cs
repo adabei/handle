11d76a5f130bca580348f37bb739a70bb0c860ad
Bernhard Schwarz <bschwrz@gmail.com>
2011-12-16 18:01:13 +0100

    adds basic functionality

--- Handle.WPF/Handle.WPF/ViewModels/ShellViewModel.cs
+++ Handle.WPF/Handle.WPF/ViewModels/ShellViewModel.cs
@@ -1,6 +1,14 @@
 ﻿namespace Handle.WPF {
   using System.ComponentModel.Composition;
+  using System;
+  using System.Windows;
+  using System.Windows.Input;
 
   [Export(typeof(IShell))]
-  public class ShellViewModel : IShell { }
+  public class ShellViewModel : IShell {
+
+    public void Exit() {
+      Application.Current.Shutdown();
+    }
+  }
 }
