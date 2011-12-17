b01b0bbcd2505d917572c588d9bbe02bbee0256d
Bernhard Schwarz <bschwrz@gmail.com>
2011-12-17 22:50:24 +0100

    makes minimizing, maximizing and restoring possible

--- Handle.WPF/Handle.WPF/ViewModels/ShellViewModel.cs
+++ Handle.WPF/Handle.WPF/ViewModels/ShellViewModel.cs
@@ -1,14 +1,51 @@
 ﻿namespace Handle.WPF {
-  using System.ComponentModel.Composition;
   using System;
+  using System.ComponentModel.Composition;
   using System.Windows;
-  using System.Windows.Input;
+  using Caliburn.Micro;
 
   [Export(typeof(IShell))]
-  public class ShellViewModel : IShell {
+  public class ShellViewModel : PropertyChangedBase, IShell {
+    private WindowState _windowState;
+
+    public WindowState WindowState {
+      get {
+        return _windowState;
+      }
+      set {
+        _windowState = value;
+        NotifyOfPropertyChange(() => WindowState);
+        NotifyOfPropertyChange(() => IsMaximized);
+        NotifyOfPropertyChange(() => IsNormal);
+      }
+    }
+
+    public bool IsNormal {
+      get {
+        return WindowState == WindowState.Normal;
+      }
+    }
+
+    public bool IsMaximized {
+      get {
+        return WindowState == WindowState.Maximized;
+      }
+    }
 
     public void Exit() {
       Application.Current.Shutdown();
+    }
+
+    public void Minimize() {
+      WindowState = WindowState.Minimized;
+    }
+
+    public void Maximize() {
+      WindowState = WindowState.Maximized;
+    }
+
+    public void Restore() {
+      WindowState = WindowState.Normal;
     }
   }
 }
