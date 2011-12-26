a851ca5566232387bcc3dd0355d2593267c4beef
Bernhard Schwarz <bschwrz@gmail.com>
2011-12-26 12:55:51 +0100

    adds basic network class

---
+++ Handle.WPF/Handle.WPF/Network.cs
@@ -0,0 +1,44 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Network.cs" company="">
+// Copyright (c) 2011 Bernhard Schwarz, Florian Lembeck
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Handle.WPF
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Text;
+
+  /// <summary>
+  /// TODO: Update summary.
+  /// </summary>
+  public class Network
+  {
+    public string Name { get; set; }
+
+    public string Address { get; set; }
+
+    public bool IsFavorite { get; set; }
+  }
+}
