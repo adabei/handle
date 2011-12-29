ea220656f5ca01a738d02ec9f4a4dc25c2d0ae02
Florian Lembeck <lembeckf@gmail.com>
2011-12-29 14:30:02 +0100

    adds shortcutview

---
+++ Handle.WPF/Handle.WPF/ViewModels/ShortcutViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace Handle.WPF.ViewModels
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Text;
+  using Handle.WPF.Views;
+  using Caliburn.Micro;
+
+  public class ShortcutViewModel
+  {
+
+    public void OK() 
+    {
+      var svm = GetView() as ShortcutView;
+      svm.DialogResult = true;
+    }
+    public void Cancel() 
+    { 
+    
+    }
+  }
+}
