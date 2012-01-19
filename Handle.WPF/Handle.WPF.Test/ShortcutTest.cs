01135e853b7bc2d9b1d19276bc6ae95d3ad8b53e
Florian Lembeck <lembeckf@gmail.com>
2012-01-19 18:47:55 +0100

    adds ShortcutTestClass

---
+++ Handle.WPF/Handle.WPF.Test/ShortcutTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using White.Core;
+using White.Core.UIItems.WindowItems;
+
+namespace Handle.WPF.Test
+{
+  [TestFixture]
+  class ShortcutTest
+  {
+    Application application;
+    [Test]
+    public void FindMainWindow() 
+    {
+      application = Application.Launch(@"C:\Users\Flotschi\git\handle\Handle.WPF\Handle.WPF\bin\Debug\Handle.WPF.exe");
+      Assert.IsNotNull(application);
+    }
+  }
+}
