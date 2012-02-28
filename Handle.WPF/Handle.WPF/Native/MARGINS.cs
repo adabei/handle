099d44370d4c13ae350c4bb8f14b0252fe4fef04
Florian Lembeck <lembeckf@gmail.com>
2012-02-28 19:53:57 +0100

    improves metro ui

---
+++ Handle.WPF/Handle.WPF/Native/MARGINS.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Handle.WPF.Native
+{
+  [StructLayout(LayoutKind.Sequential)]
+  public struct MARGINS
+  {
+    public int leftWidth;
+    public int rightWidth;
+    public int topHeight;
+    public int bottomHeight;
+  }
+}
