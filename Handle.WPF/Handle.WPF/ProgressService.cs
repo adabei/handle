2512686d81e4818603f0a62ba01770a5b7eb2be1
Bernhard Schwarz <bschwrz@gmail.com>
2012-03-04 13:42:17 +0100

    adds classes to show a progress indicator

---
+++ Handle.WPF/Handle.WPF/ProgressService.cs
@@ -0,0 +1,80 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ProgressService.cs" company="">
+// Copyright (c) 2011-2012 Bernhard Schwarz, Florian Lembeck
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Handle.WPF
+{
+  using System;
+  using System.Collections.Generic;
+  using System.ComponentModel.Composition;
+  using System.Linq;
+  using System.Text;
+  using Caliburn.Micro;
+
+  /// <summary>
+  /// TODO: Update summary.
+  /// </summary>
+  [Export(typeof(IProgressService))]
+  public class ProgressService : PropertyChangedBase, IProgressService
+  {
+    private bool isActive;
+
+    public ProgressService()
+    {
+      this.IsActive = false;
+    }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether the indicator is active or not.
+    /// </summary>
+    public bool IsActive
+    {
+      get
+      {
+        return this.isActive;
+      }
+      private set
+      {
+        this.IsActive = value;
+        NotifyOfPropertyChange(() => this.IsActive);
+      }
+    }
+
+    /// <summary>
+    /// Activates the indicator
+    /// </summary>
+    public void Show()
+    {
+      this.IsActive = true;
+    }
+
+    /// <summary>
+    /// Deactivates the indicator
+    /// </summary>
+    public void Hide()
+    {
+      this.IsActive = false;
+    }
+  }
+}
