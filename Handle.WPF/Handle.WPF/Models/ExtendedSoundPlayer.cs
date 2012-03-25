9b0304dd82a7393d1e0db9906ee5cce45c4a6c1d
Florian Lembeck <lembeckf@gmail.com>
2012-03-25 14:01:56 +0200

    adds ExtendedSoundPlayer to play sound in background

---
+++ Handle.WPF/Handle.WPF/Models/ExtendedSoundPlayer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Media;
+using System.Threading;
+using System.Windows.Threading;
+namespace Handle.WPF
+{
+  class ExtendedSoundPlayer : SoundPlayer
+  {
+    private bool myBoolIsplaying = false;
+
+    public bool bIsPlaying
+    {
+      get { return myBoolIsplaying; }
+    }
+
+    public ExtendedSoundPlayer(String strFilename) : base(strFilename) { }
+
+    public void PlaySound()
+    {
+      if (!bIsPlaying)
+      {
+        Thread threadSound = new Thread(new ThreadStart(PlaySoundThread));
+        threadSound.Start();
+      }
+    }
+
+    protected virtual void PlaySoundThread()
+    {
+      myBoolIsplaying = true;
+      //PlaySync plays the sound in the same thread and doesn't return till it is finished.
+      PlaySync();
+      myBoolIsplaying = false;
+    }
+  }
+}
